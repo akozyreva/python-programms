4add2b5eefcc84cf7c8b91ef321c40fb305cc774
akozyreva <anna.ko4523546@gmail.com>
2018-10-05 16:37:40 +0200

    add .py files

--- 7-print-fuction.py
+++ 7-print-fuction.py
@@ -4,9 +4,10 @@ Without using any string methods, try to print the following:
 123...N
 Note that ... represents the values in between.
 """
+from __future__ import print_function
 def print_function(n):
     for i in range(1, n + 1):
-        print i,
+        print (i, end=''),
 
 n = int(raw_input())
 
