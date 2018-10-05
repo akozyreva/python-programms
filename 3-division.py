b85b6235c0f5582b8484462d9198fc21b331e8a5
akozyreva <anna.ko4523546@gmail.com>
2018-10-05 16:15:24 +0200

    add .py files

---
+++ 3-division.py
@@ -0,0 +1,14 @@
+"""
+Read two integers and print two lines.
+ The first line should contain integer division,  a//b .
+  The second line should contain float division,  a/b .
+
+You don't need to perform any rounding or formatting operations
+
+"""
+
+a = raw_input()
+b = raw_input()
+
+print int(a) // int(b)
+print float(a) / float(b)
---
+++ 4-Loops.py
@@ -0,0 +1,8 @@
+"""
+Read an integer N.
+For all non-negative integers i < N, print i**2 .
+See the sample for details.
+"""
+n = int(raw_input())
+for i in range(n):
+    print i ** 2
